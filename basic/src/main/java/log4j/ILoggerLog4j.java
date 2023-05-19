3b04af4cde1cdc9a2e4251521a2d948240768362
gaopeng <gaopeng@eptcom.com>
2023-05-19 09:19:19 +0800

    log4j2 test

--- basic/src/main/java/log4j/ILoggerLog4j.java
+++ basic/src/main/java/log4j/ILoggerLog4j.java
@@ -2,10 +2,38 @@ package log4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
+
+import java.io.File;
+import java.nio.file.Files;
 
 /**
  * @author gaopeng
  */
 public interface ILoggerLog4j {
-    Logger logger = LogManager.getLogger(Log4jTest.class);
+    Logger logger = LogManager.getLogger(ILoggerLog4j.class);
+
+    /**
+     * get Logger by specified configuration of log4j2
+     *
+     * @param log4jConfPath log4j2 Configuration Path
+     * @return Logger
+     */
+    static Logger getLogger(String log4jConfPath) {
+        File log4jFile = new File(log4jConfPath);
+        try {
+            if (log4jFile.exists()) {
+                ConfigurationSource source = new ConfigurationSource(Files.newInputStream(log4jFile.toPath()), log4jFile);
+                Configurator.initialize(null, source);
+                return LogManager.getLogger("Log4j2-test");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        return null;
+    }
+
+
 }
--- basic/src/main/java/log4j/Log4jTest.java
+++ basic/src/main/java/log4j/Log4jTest.java
@@ -6,15 +6,16 @@ import org.apache.logging.log4j.*;
  * @author gaopeng
  */
 public class Log4jTest implements ILoggerLog4j {
-//	private static final Logger logger =LogManager.getLogger(Log4jTest.class);
+    //	private static final Logger logger =LogManager.getLogger(Log4jTest.class);
     public static void main(String[] args) {
+        Logger logger1 = ILoggerLog4j.getLogger("C:\\Users\\Public\\Documents\\gitlab\\java\\basic\\src\\main\\resources\\Log4j2-test.xml");
         for (int i = 0; i < 10; i++) {
-        logger.debug("调试debug信息");
-        logger.info("普通Info信息");
-        logger.warn("警告warn信息");
-        logger.error("error信息");
-        logger.fatal("严重错误fatal信息");
-        logger.trace("------------------------------------------------\n");
+            logger1.debug("调试debug信息");
+            logger1.info("普通Info信息");
+            logger1.warn("警告warn信息");
+            logger1.error("error信息");
+            logger.fatal("严重错误fatal信息");
+            logger.trace("------------------------------------------------\n");
         }
     }
 }
