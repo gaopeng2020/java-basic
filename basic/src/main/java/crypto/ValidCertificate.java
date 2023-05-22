2a7c0bf46d21bb59c5ddc83ee00fcaa2dc991058
gaopeng <gaopeng@eptcom.com>
2023-05-22 19:42:40 +0800

    Add ValidCertificate Demo

--- basic/src/main/java/crypto/ValidCertificate.java
+++ basic/src/main/java/crypto/ValidCertificate.java
@@ -2,43 +2,69 @@ package crypto;
 
 import org.apache.commons.codec.binary.Base64;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.FileInputStream;
 import java.math.BigInteger;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
-import java.util.Arrays;
 
+/**
+ * @author gaopeng
+ */
 public class ValidCertificate {
     public static void main(String[] args) throws Exception {
-        String storePath = ClassLoader.getSystemResource("keys/bo.keystore").getPath();
+//        String storePath = ClassLoader.getSystemResource("src/main/resources/server.jks").getPath();
+        String storePath = "C:\\Users\\Public\\Documents\\gitlab\\java\\basic\\src\\main\\resources\\server.jks";
         System.out.println(storePath);
 
-        PrivateKey privKey = getPrivateKey(storePath, "www.bo.org", "123456", "123456");
-        PublicKey pubKey = getPublicKey(storePath, "www.bo.org", "123456");
+        String password = "123456";
+        KeyStore ks = getKeyStore(storePath, password);
+        boolean verify = certificateVerify(ks.getCertificate("server_rsa"));
+        System.out.println("证书验证是否通过："+verify);
 
-        /*String strData = "加密前的数据";
+        PrivateKey privKey = getPrivateKey(ks, "server_rsa", password, password);
+        PublicKey pubKey = getPublicKey(ks, "server_rsa", password);
+
+        String strData = "加密前的数据";
         System.out.println("加密前数据：" + strData);
 
-        String strEnc = encrypt(strData, pubKey);
-        System.out.println("加密后的数据：" + strEnc);
+        byte[] encryptedData = encrypt(strData, pubKey);
+        System.out.println("加密后的数据：" + Base64.encodeBase64String(encryptedData));
 
-        String strDec = decrypt(strEnc, privKey);
-        System.out.println("解密后的数据：" + strDec);*/
+        byte[] decryptedData = decrypt(encryptedData, privKey);
+        assert decryptedData != null;
+        System.out.println("解密后的数据：" + new String(decryptedData));
 
         String strSignData = "签名前的数据";
-        byte[] byteSignData = strSignData.getBytes(StandardCharsets.UTF_8);
-        String signData = Arrays.toString(sign(byteSignData, privKey));
-        System.out.println("签名后的摘要信息：" + signData);
-        if (verify(byteSignData, signData, pubKey)) {
+        byte[] toSignData = strSignData.getBytes(StandardCharsets.UTF_8);
+        byte[] signedData = sign(toSignData, privKey);
+//        System.out.println("签名后的摘要信息：" + Arrays.toString(signedData));
+        if (verify(toSignData, signedData, pubKey)) {
             System.out.println("验签通过");
         } else {
             System.out.println("验签错误");
         }
+    }
+
+    private static boolean certificateVerify(Certificate certificate) {
+        boolean flag=false;
+        PublicKey publicKey = certificate.getPublicKey();
+        try {
+            certificate.verify(publicKey);
+            flag = true;
+        } catch (CertificateException | NoSuchAlgorithmException | InvalidKeyException | NoSuchProviderException |
+                 SignatureException e) {
+            throw new RuntimeException(e);
+        }
+        return flag;
     }
 
     /**
@@ -53,12 +79,12 @@ public class ValidCertificate {
         return ks;
     }
 
+
     /**
      * 通过路径获取私钥
      */
-    private static PrivateKey getPrivateKey(String keyStorePath, String alias, String storePass, String keyPass) throws Exception {
-        KeyStore ks = getKeyStore(keyStorePath, storePass);
-        PrivateKey privKey = (PrivateKey) ks.getKey(alias, keyPass.toCharArray());
+    private static PrivateKey getPrivateKey(KeyStore keyStore, String alias, String storePass, String keyPass) throws Exception {
+        PrivateKey privKey = (PrivateKey) keyStore.getKey(alias, keyPass.toCharArray());
         //返回私钥字符串
         String privateKey = Base64.encodeBase64String(privKey.getEncoded());
         System.out.println("私钥字符串：" + privateKey);
@@ -68,11 +94,10 @@ public class ValidCertificate {
     /**
      * 通过路径获取公钥
      */
-    private static PublicKey getPublicKey(String keyStorePath, String alias, String storePass) throws Exception {
-        KeyStore ks = getKeyStore(keyStorePath, storePass);
-        Certificate certificate = ks.getCertificate(alias);
+    private static PublicKey getPublicKey(KeyStore keyStore, String alias, String storePass) throws Exception {
+        Certificate certificate = keyStore.getCertificate(alias);
         PublicKey pubKey = certificate.getPublicKey();
-        BigInteger serialNumber = ((X509Certificate)certificate).getSerialNumber();
+        BigInteger serialNumber = ((X509Certificate) certificate).getSerialNumber();
         String keyId = serialNumber.toString();
         System.out.println("证书ID:" + keyId);
         //返回公钥字符串
@@ -101,34 +126,29 @@ public class ValidCertificate {
     /**
      * 用公钥加密
      */
-    private static String encrypt(String data, Key pubKey) {
-        String encData = null;
+    private static byte[] encrypt(String data, Key pubKey) {
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance(pubKey.getAlgorithm());
             cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-            byte[] encrypted = cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
-//            encData = Base64.decodeBase64(encrypted).replace("\r", "").replace("\n", "");
-            encData = Arrays.toString(Base64.decodeBase64(encrypted));
+            return cipher.doFinal(data.getBytes(StandardCharsets.UTF_8));
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return encData;
+        return null;
     }
 
     /**
      * 用私钥解密
      */
-    private static String decrypt(String data, Key privKey) {
-        String decData = null;
+    private static byte[] decrypt(byte[] encryptedData, Key privKey) {
         try {
-            Cipher cipher = Cipher.getInstance("RSA");
+            Cipher cipher = Cipher.getInstance(privKey.getAlgorithm());
             cipher.init(Cipher.DECRYPT_MODE, privKey);
-            byte[] decrypted = cipher.doFinal(Base64.decodeBase64(data));
-            decData = new String(decrypted);
+            return cipher.doFinal(encryptedData);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return decData;
+        return null;
     }
 
     /**
@@ -137,11 +157,10 @@ public class ValidCertificate {
     private static byte[] sign(byte[] data, PrivateKey privKey) throws Exception {
         MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
         messageDigest.update(data);
-        byte[] sha256 = messageDigest.digest();
-        //Signature signature = Signature.getInstance("SHA256withDSA");
-        Signature signature = Signature.getInstance("NONEwithRSA");
+        byte[] digest = messageDigest.digest();
+        Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initSign(privKey);
-        signature.update(sha256);
+        signature.update(digest);
         byte[] sign = signature.sign();
         return Base64.encodeBase64(sign);
     }
@@ -149,19 +168,13 @@ public class ValidCertificate {
     /**
      * 验签
      */
-    private static boolean verify(byte[] data, String sign, PublicKey pubKey) throws Exception {
+    private static boolean verify(byte[] data, byte[] sign, PublicKey pubKey) throws Exception {
         MessageDigest digest = MessageDigest.getInstance("SHA-256");
         digest.update(data);
-        byte[] sha256 = digest.digest();
-        //Signature signature = Signature.getInstance("SHA256withDSA");
-        Signature signature = Signature.getInstance("NONEwithRSA");
+        byte[] digested = digest.digest();
+        Signature signature = Signature.getInstance("SHA256withRSA");
         signature.initVerify(pubKey);
-        signature.update(sha256);
-        boolean pass = signature.verify(Base64.decodeBase64(sign));
-        if (!pass) {
-            return false;
-        } else {
-            return true;
-        }
+        signature.update(digested);
+        return signature.verify(Base64.decodeBase64(sign));
     }
 }
