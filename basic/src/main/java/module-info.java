7034a0822345ad8ccff067697abc59c0f0243934
gaopeng <gaopeng@e-panet.cn>
2023-05-27 23:59:19 +0800

    update basic to java17

---
+++ basic/src/main/java/module-info.java
@@ -0,0 +1,33 @@
+module basic { //在模块前使用open关键字可开放真个模块的反射权限
+    requires java.base;
+    requires java.desktop;
+    requires lombok;
+    requires org.apache.logging.log4j;
+    requires org.apache.logging.log4j.core;
+    requires transitive com.fasterxml.jackson.databind; //传递依赖，如果模块x依赖当前模块，则模块x不需要再次导入该需求模块即可直接使用
+    requires java.sql;
+    requires org.dom4j;
+    requires commons.dbcp;
+    requires commons.dbutils;
+    requires org.apache.commons.codec;
+    requires org.apache.commons.io;
+
+    exports basic.collection to javafxdemos; //只把这个包暴露给javafxdemos模块
+    exports basic.grammer;
+    opens basic.grammer; //使用open关键字可开发这个包的反射权限
+//    exports basic.oop.constructor;
+//    exports basic.oop.grammer;
+//    exports basic.reflect;
+//    exports basic.string;
+//    exports basic.thread;
+//    exports crypto;
+//    exports dbutils;
+//    exports dom4j;
+//    exports jdbc;
+//    exports json;
+//    exports log4j;
+//    exports multhread;
+//    exports proxySkeleton.server;
+//    exports rpc.client;
+//    exports rpc.server;
+}
--- basic/src/main/java/xml/XmlTest_dom4j.java
+++ basic/src/main/java/xml/XmlTest_dom4j.java
@@ -1,159 +1,155 @@
-package xml;
-
-import ept.commonapi.EPTUtils;
-import org.dom4j.*;
-import org.dom4j.io.OutputFormat;
-import org.dom4j.io.XMLWriter;
-
-import javax.swing.*;
-import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.*;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
-
-public class XmlTest_dom4j {
-    private static File lastChooserDir = null;
-
-    public static void main(String[] args) throws DocumentException, IOException, UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
-        String diretory = "D:\\Users\\Lenovo\\Documents\\DaVinci\\SAIC\\MPD_MCU\\PPV";
-        String xmlName = "MPD_1.3 - 副本.arxml";
-
-        final FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("xml(*.arxml,*.xml)", "arxml", "xml");
-        final File file = EPTUtils.fileSelector(extensionFilter);
-
-//        File file = new File(diretory + "\\" + xmlName);
-//        SAXReader saxReader = new SAXReader();
-//        final Document doc = saxReader.read(file);
-//        final Element rootElement = doc.getRootElement();
+//package xml;
 //
-//        final Element communication = getArPackagesByNameFromArxml(rootElement, "Communication");
-//        final Element signals = getElementsByNameFromArPackages(communication, "Signals");
-//        final List<Element> signalElements = getSpecifiedElementsByTypeFromElements(signals, "I-SIGNAL");
-//        for (Element signal : signalElements) {
-//            addSignalTypeForSignals(signal);
+//import ept.commonapi.EPTUtils;
+//import org.dom4j.*;
+//import org.dom4j.io.OutputFormat;
+//import org.dom4j.io.XMLWriter;
+//
+//import javax.swing.*;
+//import javax.swing.filechooser.FileNameExtensionFilter;
+//import java.io.*;
+//import java.util.Iterator;
+//import java.util.LinkedList;
+//import java.util.List;
+//
+//public class XmlTest_dom4j {
+//    private static File lastChooserDir = null;
+//
+//    public static void main(String[] args) throws DocumentException, IOException, UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+//        String diretory = "D:\\Users\\Lenovo\\Documents\\DaVinci\\SAIC\\MPD_MCU\\PPV";
+//        String xmlName = "MPD_1.3 - 副本.arxml";
+//
+//        final FileNameExtensionFilter extensionFilter = new FileNameExtensionFilter("xml(*.arxml,*.xml)", "arxml", "xml");
+//        final File file = EPTUtils.fileSelector(extensionFilter);
+//
+////        File file = new File(diretory + "\\" + xmlName);
+////        SAXReader saxReader = new SAXReader();
+////        final Document doc = saxReader.read(file);
+////        final Element rootElement = doc.getRootElement();
+////
+////        final Element communication = getArPackagesByNameFromArxml(rootElement, "Communication");
+////        final Element signals = getElementsByNameFromArPackages(communication, "Signals");
+////        final List<Element> signalElements = getSpecifiedElementsByTypeFromElements(signals, "I-SIGNAL");
+////        for (Element signal : signalElements) {
+////            addSignalTypeForSignals(signal);
+////        }
+//////        addSignalTypeForSignals(Signals);
+////        String newXmlName = "new_"+xmlName;
+////        writeXml2File(doc,diretory+"\\"+newXmlName);
+//    }
+//
+//    public static List<Element> getSpecifiedElementsByTypeFromElements(Element element, String type) {
+//        List<Element> elements = new LinkedList<>();
+//        if (element == null) {
+//            return elements;
 //        }
-////        addSignalTypeForSignals(Signals);
-//        String newXmlName = "new_"+xmlName;
-//        writeXml2File(doc,diretory+"\\"+newXmlName);
-    }
-
-    public static List<Element> getSpecifiedElementsByTypeFromElements(Element element, String type) {
-        List<Element> elements = new LinkedList<>();
-        if (element == null) {
-            return elements;
-        }
-        final Iterator<Element> iterator = element.elementIterator();
-        while (iterator.hasNext()) {
-            final Element nextElement = iterator.next();
-            if (type.equals(nextElement.getName())) {
-                elements.add(nextElement);
-            }
-        }
-        return elements;
-    }
-
-    public static void addSignalTypeForSignals(Element element) {
-        if (element == null || !"I-SIGNAL".equals(element.getName())) {
-            return;
-        }
-        //查看element是否符合添加I-SIGNAL-TYPE的条件
-        final Iterator<Element> iterator = element.elementIterator();
-        while (iterator.hasNext()) {
-            final String elementType = iterator.next().getName();
-            if ("DATA-TRANSFORMATIONS".equals(elementType) || "TRANSFORMATION-I-SIGNAL-PROPSS".equals(elementType)
-                    || "I-SIGNAL-TYPE".equals(elementType)) {
-                return;
-            }
-        }
-        //将I-SIGNAL-TYPE插入到DATA-TYPE-POLICY的后面
-        int order = 0;
-        final Iterator<Element> it = element.elementIterator();
-        while (it.hasNext()) {
-            final String elementType = it.next().getName();
-            if ("SHORT-NAME".equals(elementType) || "DESC".equals(elementType)
-                    || "LONG-NAME".equals(elementType) || "DATA-TYPE-POLICY".equals(elementType)) {
-                order++;
-            }
-        }
-        //add QName,to remove xmlns attribute
-        final QName qName = QName.get("I-SIGNAL-TYPE", element.getNamespace());
-        final Element signalTypeElement = DocumentHelper.createElement(qName);
-
-        signalTypeElement.setText("PRIMITIVE");
-        signalTypeElement.remove(signalTypeElement.attribute("xmlns"));
-        element.elements().add(order, signalTypeElement);
-    }
-
-    public static Element getElementByNameFromElements(Element element, String name) {
-        if (element == null || "".equals(name)) {
-            return null;
-        }
-        final Iterator<Element> elementsIterator = element.elementIterator();
-        while (elementsIterator.hasNext()) {
-            final Element subElement = elementsIterator.next();
-            if (name.equals(subElement.element("SHORT-NAME").getText())) {
-                return subElement;
-            }
-        }
-        return null;
-    }
-
-    public static Element getElementsByNameFromArPackages(final Element element, String name) {
-        if (element == null || "".equals(name)) {
-            return null;
-        }
-        final Iterator<Element> arPackageElementIterator = element.elementIterator();
-        while (arPackageElementIterator.hasNext()) {
-            final Element arPackageElement = arPackageElementIterator.next();
-            if (name.equals(arPackageElement.element("SHORT-NAME").getText())) {
-                return arPackageElement.element("ELEMENTS");
-            }
-        }
-        return null;
-    }
-
-    public static Element getArPackagesByNameFromArxml(final Element element, String name) {
-        if (element == null || "".equals(name)) {
-            return null;
-        }
-        final Element arPackagesElement = element.element("AR-PACKAGES");
-        final Iterator<Element> arPackagesElementIterator = arPackagesElement.elementIterator();
-        while (arPackagesElementIterator.hasNext()) {
-            final Element arPackageElement = arPackagesElementIterator.next();
-            if (name.equals(arPackageElement.element("SHORT-NAME").getText())) {
-                return arPackageElement.element("AR-PACKAGES");
-            }
-        }
-        return null;
-    }
-
-    public static void writeXml2File(Document document, String filePath) {
-        File xmlFile = new File(filePath);
-        XMLWriter writer = null;
-        final OutputFormat outputFormat = OutputFormat.createPrettyPrint();
-        outputFormat.setEncoding("UTF-8");
-        try {
-            if (xmlFile.exists()) {
-                xmlFile.delete();
-            }
-            writer = new XMLWriter(new FileOutputStream(xmlFile), outputFormat);
-            writer.write(document);
-            writer.close();
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-}
+//        final Iterator<Element> iterator = element.elementIterator();
+//        while (iterator.hasNext()) {
+//            final Element nextElement = iterator.next();
+//            if (type.equals(nextElement.getName())) {
+//                elements.add(nextElement);
+//            }
+//        }
+//        return elements;
+//    }
+//
+//    public static void addSignalTypeForSignals(Element element) {
+//        if (element == null || !"I-SIGNAL".equals(element.getName())) {
+//            return;
+//        }
+//        //查看element是否符合添加I-SIGNAL-TYPE的条件
+//        final Iterator<Element> iterator = element.elementIterator();
+//        while (iterator.hasNext()) {
+//            final String elementType = iterator.next().getName();
+//            if ("DATA-TRANSFORMATIONS".equals(elementType) || "TRANSFORMATION-I-SIGNAL-PROPSS".equals(elementType)
+//                    || "I-SIGNAL-TYPE".equals(elementType)) {
+//                return;
+//            }
+//        }
+//        //将I-SIGNAL-TYPE插入到DATA-TYPE-POLICY的后面
+//        int order = 0;
+//        final Iterator<Element> it = element.elementIterator();
+//        while (it.hasNext()) {
+//            final String elementType = it.next().getName();
+//            if ("SHORT-NAME".equals(elementType) || "DESC".equals(elementType)
+//                    || "LONG-NAME".equals(elementType) || "DATA-TYPE-POLICY".equals(elementType)) {
+//                order++;
+//            }
+//        }
+//        //add QName,to remove xmlns attribute
+//        final QName qName = QName.get("I-SIGNAL-TYPE", element.getNamespace());
+//        final Element signalTypeElement = DocumentHelper.createElement(qName);
+//
+//        signalTypeElement.setText("PRIMITIVE");
+//        signalTypeElement.remove(signalTypeElement.attribute("xmlns"));
+//        element.elements().add(order, signalTypeElement);
+//    }
+//
+//    public static Element getElementByNameFromElements(Element element, String name) {
+//        if (element == null || "".equals(name)) {
+//            return null;
+//        }
+//        final Iterator<Element> elementsIterator = element.elementIterator();
+//        while (elementsIterator.hasNext()) {
+//            final Element subElement = elementsIterator.next();
+//            if (name.equals(subElement.element("SHORT-NAME").getText())) {
+//                return subElement;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public static Element getElementsByNameFromArPackages(final Element element, String name) {
+//        if (element == null || "".equals(name)) {
+//            return null;
+//        }
+//        final Iterator<Element> arPackageElementIterator = element.elementIterator();
+//        while (arPackageElementIterator.hasNext()) {
+//            final Element arPackageElement = arPackageElementIterator.next();
+//            if (name.equals(arPackageElement.element("SHORT-NAME").getText())) {
+//                return arPackageElement.element("ELEMENTS");
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public static Element getArPackagesByNameFromArxml(final Element element, String name) {
+//        if (element == null || "".equals(name)) {
+//            return null;
+//        }
+//        final Element arPackagesElement = element.element("AR-PACKAGES");
+//        final Iterator<Element> arPackagesElementIterator = arPackagesElement.elementIterator();
+//        while (arPackagesElementIterator.hasNext()) {
+//            final Element arPackageElement = arPackagesElementIterator.next();
+//            if (name.equals(arPackageElement.element("SHORT-NAME").getText())) {
+//                return arPackageElement.element("AR-PACKAGES");
+//            }
+//        }
+//        return null;
+//    }
+//
+//    public static void writeXml2File(Document document, String filePath) {
+//        File xmlFile = new File(filePath);
+//        XMLWriter writer = null;
+//        final OutputFormat outputFormat = OutputFormat.createPrettyPrint();
+//        outputFormat.setEncoding("UTF-8");
+//        try {
+//            if (xmlFile.exists()) {
+//                xmlFile.delete();
+//            }
+//            writer = new XMLWriter(new FileOutputStream(xmlFile), outputFormat);
+//            writer.write(document);
+//            writer.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        } finally {
+//            if (writer != null) {
+//                try {
+//                    writer.close();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+//}
