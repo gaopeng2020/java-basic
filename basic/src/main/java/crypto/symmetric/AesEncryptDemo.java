4050d1e0e01394467d2ea07313cc4101079cfc9c
gaopeng <gaopeng@e-panet.cn>
2023-05-28 19:29:13 +0800

    update AesEncryptDemo

--- basic/src/main/java/crypto/symmetric/AesEncryptDemo.java
+++ basic/src/main/java/crypto/symmetric/AesEncryptDemo.java
@@ -4,22 +4,29 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
+import java.security.SecureRandom;
 import java.util.Base64;
 
 /**
  * @author gaopeng
  */
 public class AesEncryptDemo {
-    // DES加密算法,key的大小必须是8个字节
-
+    /**
+     * 初始向量IV, 初始向量IV的长度规定为128位16个字节, 初始向量的来源为openssl随机生成(openssl rand -base64 16).
+     */
+    private static final byte[] KEY_VI = Base64.getDecoder().decode("muN/4hiPz8mxxIiKBVgTtg==");
 
     public static void main(String[] args) throws Exception {
         String input = "AES加密算法测试 : AES/CBC/PKCS5Padding";
-        // AES加密算法，key的大小必须是16个字节
-        String key = "A1B2C3D4E5F6G7H8";
+        System.out.println("加密前数据 = " + input);
+
+        // AES加密算法，密钥的长度可以是128位16个字节、192位（24字节）或者256位(32字节),
+        // 密码可以随机生成也可以手动指定，例如String key = "A1B2C3D4E5F6G7H8";
+        byte[] key =  new SecureRandom().generateSeed(32);
+        System.out.println("key = " + Base64.getEncoder().encodeToString(key));
 
         // CBC模式,必须指定初始向量,初始向量中密钥的长度需符合算法标准，DES为8字节，AES为16字节
-        String transformation = "AES/CBC/PKCS5Padding";
+         String transformation = "AES/CBC/PKCS5Padding";
         // 指定获取密钥的算法
         String algorithm = "AES";
 
@@ -39,16 +46,16 @@ public class AesEncryptDemo {
      * @param algorithm      : 获取密钥的算法
      * @return : 密文
      */
-    private static String encryptAES(String input, String key, String transformation, String algorithm) throws Exception {
+    private static String encryptAES(String input, byte[] key, String transformation, String algorithm) throws Exception {
         // 获取加密对象
         Cipher cipher = Cipher.getInstance(transformation);
         // 创建加密规则
         // 第一个参数key的字节
         // 第二个参数表示加密算法
-        SecretKeySpec sks = new SecretKeySpec(key.getBytes(), algorithm);
+        SecretKeySpec sks = new SecretKeySpec(key, algorithm);
 
         //初始向量，参数表示跟谁进行异或，初始向量的长度必须是8位
-        IvParameterSpec iv = new IvParameterSpec(key.getBytes());
+        IvParameterSpec iv = new IvParameterSpec(KEY_VI);
 
         // ENCRYPT_MODE：加密模式
         // DECRYPT_MODE: 解密模式
@@ -69,14 +76,14 @@ public class AesEncryptDemo {
      * @param algorithm      : 获取密钥的算法
      * @return: 原文
      */
-    private static String decryptAES(String input, String key, String transformation, String algorithm) throws Exception {
+    private static String decryptAES(String input, byte[] key, String transformation, String algorithm) throws Exception {
         // 1,获取Cipher对象
         Cipher cipher = Cipher.getInstance(transformation);
         // 指定密钥规则
-        SecretKeySpec sks = new SecretKeySpec(key.getBytes(), algorithm);
+        SecretKeySpec sks = new SecretKeySpec(key, algorithm);
 
-        //初始向量，参数表示跟谁进行异或，初始向量的长度必须是8位
-        IvParameterSpec iv = new IvParameterSpec(key.getBytes());
+        //初始向量，参数表示跟谁进行异或，初始向量的长度必须是16字节
+        IvParameterSpec iv = new IvParameterSpec(KEY_VI);
 
         cipher.init(Cipher.DECRYPT_MODE, sks, iv);
         // 3. 解密，上面使用的base64编码，下面直接用密文
